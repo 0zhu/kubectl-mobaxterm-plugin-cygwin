a022490178fc77c861f35d427c7f699df35ef763
zhubanRuban <30699069+zhubanRuban@users.noreply.github.com>
2020-09-16 17:57:56 +0300

    first commit

---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# based on https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-on-windows
+
+# amd64 for 64-bit / 386 for 32-bit
+KBIT=386
+KEXE=/bin/kubectl
+KPLUGIN=kubectl.mxt3
+KBAK="$KEXE".bakmxtbuild
+
+set -e
+
+hash curl zip
+
+if [ -f "$KEXE" ]; then
+    KEXIST=1
+    mv "$KEXE" "$KBAK"
+    echo "$KEXE" exists, renamed to "$KBAK" during plugin build
+fi
+
+KLATEST=$( curl -skL https://storage.googleapis.com/kubernetes-release/release/stable.txt )
+echo Latest kubectl release: "$KLATEST"
+curl -kLo "$KEXE" https://storage.googleapis.com/kubernetes-release/release/"$KLATEST"/bin/windows/"$KBIT"/kubectl.exe
+zip "$KPLUGIN" "$KEXE"
+echo Plugin file: $( realpath "$KPLUGIN" )
+
+if [ "$KEXIST" ]; then
+    mv -f "$KBAK" "$KEXE"
+    echo "$KBAK" renamed back to "$KEXE"
+else
+    echo Deleting "$KEXE" used for plugin build...
+    rm -f "$KEXE" 
+fi
+
+echo Done.
